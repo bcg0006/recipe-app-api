e78203658fe8a54bc4cacb44f04459eaeeaebff0
Bruno <bcg0006@gmail.com>
2024-04-06 08:13:42 +0200

    Added testing for new function

---
+++ app/app/tests.py
@@ -0,0 +1,25 @@
+"""
+This file contains the tests for the app.   
+"""
+
+from django.test import SimpleTestCase
+from app import calc 
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add(self):
+        """Test the add function """
+        self.assertEqual(calc.add(3, 8), 11)
+        self.assertEqual(calc.add(-3, 8), 5)
+        self.assertEqual(calc.add(-3, -8), -11)
+        self.assertEqual(calc.add(3, -8), -5)
+
+class SubstractNumbers(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add(self):
+        """Test the substract function """
+        res = calc.subtract(5, 11)
+        self.assertEqual(res, -6)
+       
