536e9334949da956f8ed7e34cc3e6dd7e4dfc66c
Bruno <bcg0006@gmail.com>
2024-04-13 11:30:29 +0200

    earlier changes - before starting user model implementation

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,14 +1,27 @@
 """
 Django to wait to wait for the database to be available before continuing with the execution of the command.
 """
+import time
+from psycopg2 import OperationalError as Psycopg2OpError
 
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        """Handle the command"""
-        pass 
-        #self.stdout.write('Waiting for database...')
-        #self.stdout.write('Database available!')
+        """Entry point for command""" 
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
