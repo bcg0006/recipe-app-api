9c190b53dc004e90f0e34af79b61136fe6f5019f
Bruno <bcg0006@gmail.com>
2024-04-20 11:30:24 +0200

    added suepr user functionality and email verification

--- app/core/models.py
+++ app/core/models.py
@@ -10,9 +10,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Create and return a new user """
+        if not email:
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password) # encrypts the password
         user.save(using=self._db) # using=self._db is for supporting multiple databases
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ Create and return a new superuser """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
