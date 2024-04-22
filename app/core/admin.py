d73e9831875dcc8413dbf2a486dba204cfd61a93
Bruno <bcg0006@gmail.com>
2024-04-22 14:46:16 +0200

    added user admin interface

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,31 @@
-from django.contrib import admin # noqa
+"""Django Admin customization for the core app."""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Customize the user admin page."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser'),
+            }
+        ),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
